9fd5f95e8538009c0a23e132cb2956b2795fef1e
Debosmita Bhattacharjee <78953484+theLIT02@users.noreply.github.com>
2021-09-30 17:18:29 +0530

    Add files via upload

---
+++ sumof2num.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    
+    cout << "Enter the 1st number: " ;
+    cin>> a;
+    cout << "Enter the 2nd number: " ;
+    cin>>b;
+    cout<< "The sum is:" << a+b << endl;
+    return 0;
+}
